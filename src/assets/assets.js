d5649cf71107b7230227e24d70e57366fd434e4d
Samuel Frías Nieto <sae.nieto@gmail.com>
2025-02-25 11:23:35 +0100

    initialize React app with TypeScript, Vite, and Tailwind CSS setup

---
+++ src/assets/assets.js
@@ -0,0 +1,14 @@
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "Women Round Neck Cotton Top",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting top",
+        price: 100,
+        image: [],
+        category: "Women",
+        subCategory: "Topwear",
+        sizes: ["S","M","L"],
+        date: 1716634345448,
+        bestseller: true
+    }
+]
